f2d0e216e7f88611305a2ef8044d46de9d6c506d
Seth0r <github@seth0r.net>
2019-09-16 16:24:38 +0200

    adding persitent packages and ssh config

--- 16.04/files/run.sh
+++ 16.04/files/run.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-for f in passwd shadow; do
+for f in passwd shadow group ssh; do
+    if [ ! -f /config/$f.init ]; then
+        cp -av /etc/$f /etc/$f.init
+    fi
     if [ ! -f /config/$f ]; then
-        cp /etc/$f.init /config/$f
+        cp -av /etc/$f.init /config/$f
     fi
     if [ ! -L /etc/$f ]; then
-        rm /etc/$f
+        rm -rf /etc/$f
         ln -s /config/$f /etc/$f
     fi
 done
-
+apt-get update
+apt-get dist-upgrade -y
+if [ -f /config/install ]; then
+    cat /config/install | while read p; do
+        apt-get install -y "$p"
+    done
+fi
+if [ -f /config/remove ]; then
+    cat /config/remove | while read p; do
+        apt-get remove -y "$p"
+    done
+fi
+apt-get autoremove -y
+if [ -f /config/run.sh ]; then
+    bash /config/run.sh
+fi
 /usr/sbin/sshd -D
