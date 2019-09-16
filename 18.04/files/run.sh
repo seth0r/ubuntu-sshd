12e129651484a5e0ae37cccd2e0f06f297ebefdc
Seth0r <github@seth0r.net>
2019-09-16 15:25:09 +0200

    more testing

--- 18.04/files/run.sh
+++ 18.04/files/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+for f in passwd shadow; do
+    if [ ! -f /config/$f ]; then
+        cp /etc/$f.init /config/$f
+    fi
+    if [ ! -L /etc/$f ]; then
+        rm /etc/$f
+        ln -s /config/$f /etc/$f
+    fi
+done
 
 /usr/sbin/sshd -D
